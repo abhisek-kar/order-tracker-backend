87d2d13f6a0b768beddf76dc2c46128ad2760978
abhisek-kar <abhi.originals.001@gmail.com>
2025-08-11 21:58:41 +0530

    feat: Set up global error handler and logger

--- src/config/env.ts
+++ src/config/env.ts
@@ -6,6 +6,7 @@ dotenv.config();
 const envSchema = z.object({
   PORT: z.string().default("5000").transform(Number),
   MONGO_URI: z.string().url({ message: "Invalid MongoDB connection string" }),
+  NODE_ENV: z.enum(["development", "production"]).default("development"),
 });
 
 const appEnv = envSchema.parse(process.env);
---
+++ src/middlewares/errorHandler.ts
@@ -0,0 +1,15 @@
+import { Request, Response, NextFunction } from "express";
+import logger from "../utils/logger";
+
+export const errorHandler = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  logger.error(err.stack);
+
+  res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+  });
+};
